273b5af98ee6e1f466f71268d8fa4f17e69818ce
marianovungel <vungem88@gmail.com>
2023-03-06 02:52:59 -0300

    organizando ao Ambiente

---
+++ Screens/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import tw from "tailwind-react-native-classnames";
+
+const Home = () => {
+  return (
+    <View style={tw`pt-8 ml-3`}>
+      <Text>Home Mariano Uber</Text>
+    </View>
+  )
+}
+
+export default Home;
+
+const styles = StyleSheet.create({})
